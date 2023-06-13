1e5c7b989cef07b6040de1fd5bfed357fd65fd9f
andbardii <andbardii@icloud.com>
2023-06-13 09:20:20 +0200

    add @angular/fire

--- Epibook/src/app/app.module.ts
+++ Epibook/src/app/app.module.ts
@@ -7,6 +7,17 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
@@ -16,8 +27,20 @@ import { FormsModule } from '@angular/forms';
     NgbModule,
     HttpClientModule,
     FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Epibook/src/environments/environment.ts
+++ Epibook/src/environments/environment.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'epibooksocial',
+    appId: '1:770668529357:web:56a3a479eb8a8e76b14b84',
+    storageBucket: 'epibooksocial.appspot.com',
+    apiKey: 'AIzaSyCG3OLDzrHaCnfgG0MvBeiiAf1F6LuuCns',
+    authDomain: 'epibooksocial.firebaseapp.com',
+    messagingSenderId: '770668529357',
+    measurementId: 'G-PKM4Q2WEQ6',
+  },};
