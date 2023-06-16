5250b1cdc786224c22ea168eb70e4d89213e4b78
VSalzano <vincenzo.salzano@outlook.com>
2023-06-16 09:30:04 +0200

    correzioni

--- Epibook/src/app/Pages/auth/register/register.component.ts
+++ Epibook/src/app/Pages/auth/register/register.component.ts
@@ -7,39 +7,34 @@ import { AuthService } from 'src/app/Serivices/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-user: ILogin = {
-  email: '',
-  password: ''
-}
+  user: ILogin = {
+    email: '',
+    password: '',
+  };
 
-newUser:IUser = {
-  username: '@username',
-  email: '',
-  profilePic: 'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png',
-  description: 'Description',
-  followerArr:[],
-  followArr:[]
-}
+  newUser: IUser = {
+    username: '@username',
+    email: '',
+    profilePic:
+      'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png',
+    description: 'Description',
+    followerArr: ['-NY1yKfsgmYX8PG1nIiO'],
+    followArr: ['-NY1yKfsgmYX8PG1nIiO'],
+  };
 
+  constructor(private authSvc: AuthService, private router: Router) {}
 
-constructor(
-  private authSvc:AuthService,
-  private router:Router
-){}
-
-
-register(){
-this.newUser.email = this.user.email
-this.authSvc.register(this.user)
-.subscribe(data =>{
-  console.log(data);
-  this.router.navigate(['/auth', 'login']);
-});
-this.authSvc.registerData(this.newUser).subscribe(data =>{
-  console.log("Utente registrato nel Database");
-})
-}
+  register() {
+    this.newUser.email = this.user.email;
+    this.authSvc.register(this.user).subscribe((data) => {
+      console.log(data);
+      this.router.navigate(['/auth', 'login']);
+    });
+    this.authSvc.registerData(this.newUser).subscribe((data) => {
+      console.log('Utente registrato nel Database');
+    });
+  }
 }
